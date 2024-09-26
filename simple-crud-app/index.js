2a0d999992bf5dd1c4850a374b794e96e1a9c2fc
alsongard <alsongard@gmail.com>
2024-09-26 04:48:37 -0400

    setup express successfull and request,respone working

--- simple-crud-app/index.js
+++ simple-crud-app/index.js
@@ -1,1 +1,11 @@
-console.log("Hello There");
+// console.log("Hello There");
+
+const express = require("express");
+const app = express();
+
+app.listen(3000, ()=>{ console.log("Server is running on port 3000");});
+
+
+app.get("/", (req,res)=>{
+    res.send("Node Api Running : running till now : Successfully installed nodemon : Only refresh on browser");
+});
