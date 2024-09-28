8ea1ad7b37c5ae545da619dc32b142becf57af20
alsongard <alsongard@gmail.com>
2024-09-28 03:42:00 -0400

    need more info on this

--- simple-crud-app/index.js
+++ simple-crud-app/index.js
@@ -2,13 +2,21 @@
 
 const express = require("express");
 const mongoose = require('mongoose');
+
 const app = express();
 
+app.use(express.json());
 
 
 app.get("/", (req,res)=>{
     res.send("Node Api Running : running till now : Successfully installed nodemon : Only refresh on browser");
 });
+
+
+app.post("/api/products", (req, res)=>{
+    console.log(req.body); //from user
+    res.send(req.body);
+})
 
 mongoose.connect("mongodb+srv://alsongard:UBsTr9zfJlQSE07E@backend.brhgn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Backend")
 .then( ()=>{
