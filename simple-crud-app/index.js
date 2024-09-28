7eab60d36658b02a5d10373207af3ccbf75a7537
alsongard <alsongard@gmail.com>
2024-09-28 02:22:21 -0400

    mongodb, mongoose  setup and functional

--- simple-crud-app/index.js
+++ simple-crud-app/index.js
@@ -1,11 +1,18 @@
 // console.log("Hello There");
 
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
 
-app.listen(3000, ()=>{ console.log("Server is running on port 3000");});
 
 
 app.get("/", (req,res)=>{
     res.send("Node Api Running : running till now : Successfully installed nodemon : Only refresh on browser");
 });
+
+mongoose.connect("mongodb+srv://alsongard:UBsTr9zfJlQSE07E@backend.brhgn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Backend")
+.then( ()=>{
+    app.listen(3000, ()=>{ console.log("Server is running on port 3000");});
+    console.log("Connected to the database");
+})
+.catch(error => console.log(error));
