ceb457da788304fff7cde437fc7eac686ce36ee9
alsongard <alsongard@gmail.com>
2024-12-19 00:15:05 +0300

    created new file

--- tutorials/15-create-big-file.js
+++ tutorials/15-create-big-file.js
@@ -3,3 +3,8 @@ for(let i = 0; i < 10000; i++)
 {
     writeFileSync("./content/big.txt", `hello Earth ${i} \n`, {flag: 'a'})
 }
+
+for(let i = 0; i < 1000000; i++)
+{
+    writeFileSync("./content/new_big.txt", `Hi welcome to universe ${i} \n`, {flag: 'a'})
+}
