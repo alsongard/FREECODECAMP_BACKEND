0f6992e1ce6cf464da31e8aae99eda76e85c8aa4
alsongard <alsongard@gmail.com>
2024-12-21 22:20:44 +0300

    using response.sendFile, requires other links to be provided

---
+++ tutorials/express-tutorial/3-express-defect.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require("path")
+const app = express() // const server = http.createServer()
+
+app.get("/", (request, response)=>{
+    response.status(200).send("Home Page")
+})
+app.get("/index.html", (request, response)=>{
+    response.sendFile(path.join(__dirname, "html_files", "index.html"))
+})
+app.get("/about", (request, response)=>{
+    response.status(200).status.send("<h1>Welcome to About Page </h1>")
+})
+app.get("*", (request, response)=>{
+    response.status(404).send("<h1> Cannot find Page</h1>")
+})
+
+
+
+// app.get | display
+// app.post | insert
+// app.delete | delete
+// app.put  | update
+// app.all |
+// app.use | middleware 
+// app.listen
+
+
+app.listen(5000, ()=>{
+    console.log("Listening on port 5000..")
+})
+
