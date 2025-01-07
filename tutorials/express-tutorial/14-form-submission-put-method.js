85d0d3b8574895d44cb8fd37b49b1217f5840600
alsongard <alsongard@gmail.com>
2025-01-07 22:25:10 +0300

    updating data is done

---
+++ tutorials/express-tutorial/14-form-submission-put-method.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const {people}  = require("./new_data.js");
+const app = express();
+const path = require("path");
+const exp = require("constants");
+const { type } = require("os");
+
+// console.log(people);
+// console.log(typeof(people))
+// console.log(module);
+app.use(express.static(path.join(__dirname, "methods_public")));  // middleware used for static pages
+app.use(express.urlencoded({extended: false})); // middleware used for traditional form
+app.use(express.json()) // another inbuilt middleware used for javascript form
+
+
+
+app.get("/api/people",(request,response)=>{
+    console.log(people);
+    response.status(200).json({success: true, data:people})
+});
+
+
+app.post("/api/people", (request, response)=>{
+    console.log("this is request body: ")
+    console.log(request.body)
+    const {name} = request.body;
+    // console.log(`Type of request body is : ${request.body} and data is : ${request.body}`)
+    console.log(name); // prints out the value inpute by the user as an object
+    if (name)
+    {
+        response.status(201).json({sucess:true, data: name})
+    }
+    else{
+        response.status(400).json({success:false, msg:"please provide value"})
+    }
+});
+
+
+app.post("/api/postman/people", (request, response)=>{
+    console.log(`Type of poepleData is : ${typeof(peopleData)}`)
+    const {name} = request.body;
+    console.log(request.body);
+    console.log(name);
+    if (name)
+    {
+        response.status(200).send({sucess: true, data :[...people, name]});
+    }
+    else
+    {
+        response.status(400).send({sucess: false, msg: "please provide a name"})
+    }
+})
+app.post("/login", (request, response)=>{
+    // console.log(request.body) // prints [Object: null prototype] { fullName: 'Arcade VI Jinx' }
+    const {fullName} = request.body;
+    // response.send("<p>POST working</p>");
+    if (fullName)
+    {
+        response.status(200).send(`My body is on the line now i can't feel this time now from  ${fullName}`)
+    }
+    else{
+        response.status(404).send("<p>Enter full name </p>");
+    }
+})
+
+app.put("/api/people/:id", (request, response)=>{
+    const {id} = request.params;
+    const {names} = request.body;
+    console.log(id);
+    console.log(typeof(id))
+    // perform type  conversion
+    const new_id = Number(id);
+
+
+    const personFound =  people.find((peopleItem)=>{
+        if (peopleItem.id === new_id)
+        {
+            return peopleItem.name = names;
+        }
+    })
+    console.log(personFound);
+
+    if (!personFound)
+    {
+        response.status(400).send({success: false, msg: "Person Not found"})
+    }
+    else{
+
+        response.status(200).send(people)
+    }
+})
+
+app.listen(5000, (request, response)=>{
+    console.log("Listenning on port 5000...");
+});
