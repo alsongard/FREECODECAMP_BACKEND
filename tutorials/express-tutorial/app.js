b0bb71d5ce7fc9e04d390a67858eecd5652a3de6
alsongard <alsongard@gmail.com>
2025-01-07 23:19:22 +0300

    update on put,delete methods

--- tutorials/express-tutorial/app.js
+++ tutorials/express-tutorial/app.js
@@ -41,28 +41,86 @@ app.post("/api/postman/people", (request, response)=>{
     const {name} = request.body;
     console.log(request.body);
     console.log(name);
-    if (name)
+    if (!name)
     {
-        response.status(200).send({sucess: true, data :[...people, name]});
+        return response.status(400).send({sucess: false, msg: "please provide a name"})
     }
     else
     {
-        response.status(400).send({sucess: false, msg: "please provide a name"})
+        response.status(200).send({sucess: true, data :[...people, name]});
+
     }
 })
 app.post("/login", (request, response)=>{
     // console.log(request.body) // prints [Object: null prototype] { fullName: 'Arcade VI Jinx' }
     const {fullName} = request.body;
     // response.send("<p>POST working</p>");
-    if (fullName)
+    if (!fullName)
     {
-        response.status(200).send(`My body is on the line now i can't feel this time now from  ${fullName}`)
+        return response.status(404).send("<p>Enter full name </p>");
+
     }
     else{
-        response.status(404).send("<p>Enter full name </p>");
+        response.status(200).send(`My body is on the line now i can't feel this time now from  ${fullName}`)
     }
 })
 
-app.listen(5000, (request, response)=>{
+app.put("/api/people/:id", (request, response)=>{
+    const {id} = request.params;
+    const {names} = request.body;
+    console.log(id);
+    console.log(typeof(id))
+    // perform type  conversion
+    const new_id = Number(id);
+
+    const personFound =  people.find((peopleItem)=>{
+        if (peopleItem.id === new_id)
+        {
+            return peopleItem.name = names;
+        }
+    })
+    console.log(personFound);
+
+    if (!personFound)
+    {
+        return response.status(400).send({success: false, msg: "Person Not found"})
+    }
+    else{
+
+        response.status(200).json({success: true, data: people})
+    }
+})
+
+
+
+app.delete("/api/people/:id", (request, response)=>{
+    const {id} = request.params;
+    const new_id = Number(id);
+
+    const person = people.find((peopleItem)=>{
+        if (peopleItem.id === new_id)
+            {
+                return peopleItem;
+            }
+    });
+
+    // const newPeople = people.filter(peopleItem => peopleItem.id !== new_id)
+    const newPeople = people.filter((peopleItem)=>{
+        if (peopleItem.id !== new_id)
+        {
+            return peopleItem;
+        }
+    })
+    console.log(newPeople);
+        
+    if (!person)
+    {
+        return response.status(400).send({success:false, msg:`User with id ${id} not found `} )
+    }
+    response.status(200).json({success: true, data: newPeople})
+})
+
+
+app.listen(5000, ()=>{
     console.log("Listenning on port 5000...");
 });
