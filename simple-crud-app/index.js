c9e516d0ff4c8002b97d018b840b461bac6982aa
alsongard <alsongard@gmail.com>
2025-02-06 14:07:03 +0200

    fetching all product data and by Id successfull

---
+++ simple-crud-app/index.js
@@ -0,0 +1,71 @@
+// console.log("Hello There");
+require("dotenv").config();
+const mongoose = require('mongoose');
+const express = require("express");
+const Product = require("./models/product.model");
+
+const app = express();
+app.use(express.json());
+
+const username = process.env.username;
+const password = process.env.password;
+
+app.get("/", (request,response)=>{
+    response.send("Node Api Running : running till now : Successfully installed nodemon : Only refresh on browser");
+});
+
+//  fetch all products
+app.get("/api/products", async (request, response)=>{
+    try{
+        const products = await Product.find({});
+        response.status(200).json(products);
+    }
+    catch(error)
+    {
+        response.status(500).send(`Error : ${error.message}`);
+    }
+})
+
+
+//fetch a specific product
+app.get("/api/product/:id",async (request, response)=>{
+    const myProductId = request.params.id;
+    console.log(`Product Id : ${myProductId}`);
+    try{
+        const singleProduct = await Product.findById(myProductId);
+        // if (!singleProduct)
+        // {
+        //     response.status(200).json({success:true, msg: "No match found with the product id"});
+        // }
+        response.status(200).json({success: true, data: singleProduct})
+    }
+    catch(error){
+        response.status(500).send(`Error: ${error.message}`)
+    }
+})
+
+app.post("/api/products", async (request, response)=>{
+    // console.log(request.body)
+    // response.send(request.body)
+    try
+    {
+        const product = await Product.create(request.body);
+        response.status(200).json(product);
+    }
+    catch(error)
+    {
+        res.status(500).json({success: false, message: message.error}); //status 500 is a server error
+    }
+})
+                // "mongodb+src://userName:password@clusterName.nrurtot.mongodb.net/NODE-API?retryWrites=true&w="
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.g3vdrwa.mongodb.net/NODE-API?retryWrites=true&w=majority&appName=Cluster`)
+.then( ()=>{
+    console.log("Connected to the database");
+    app.listen(3000, ()=>{ console.log("Server is running on port 3000");});
+})
+.catch(error => console.log(error));
+
+
+app.listen(5000, ()=>{
+    console.log("Listening on port 5000");
+})
